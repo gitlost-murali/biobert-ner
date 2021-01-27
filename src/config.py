80bd800604fbe7747e503f07d0b262bae8917c2f
gitlost-murali <kmanoharmurali@gmail.com>
2021-01-27 18:10:37 +0530

    Script update: config.py: Config details updated

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@ import transformers
 from transformers import AutoTokenizer
 
 MAX_LEN = 128
-TRAIN_BATCH_SIZE = 4
-VALID_BATCH_SIZE = 2
+TRAIN_BATCH_SIZE = 12
+VALID_BATCH_SIZE = 4
 EPOCHS = 10
 CUDA = True
-BASE_MODEL_PATH = "../input/ixambert-base-cased" #bert-base-uncased OR biobert-large-cased-v1.1
+BASE_MODEL_PATH = "../input/Bio_ClinicalBERT" #bert-base-uncased OR biobert-large-cased-v1.1
 MODEL_PATH = "model.bin" #biomodel_on_custom.bin OR model.bin
 BASE_MODEL_DIM = 768 # 1024 for bioBERT & 768 for BERT
-TRAINING_FILE = "../input/train.tsv" # Use the function process_data_conll()
+TRAINING_FILE = "../../../datasets/experiment_data_en/english_protein_chemical.json" # Use the function process_data_conll()
 # TRAINING_FILE = "../input/dldata_bilou_7k.json" # Use the function read_bilou
 TOKENIZER = AutoTokenizer.from_pretrained(BASE_MODEL_PATH)
 # TOKENIZER = transformers.BertTokenizer.from_pretrained(
